f6570b893e0c33bc26dcb7ea27d5c05449d4d5b2
Isaac-Saleh <118036130+Isaac-Saleh@users.noreply.github.com>
2023-12-07 14:21:44 +0800

    Day3 complete after many refactors

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -1,58 +1,59 @@
-with open("puzzle_input_day3.txt", 'r') as file:
+with open("puzzle_input_day3.txt") as file:
     lines = file.readlines()
 
+def look_for_symbols(si, ei, index):
+    row = grid[index]
+    # for cell in row:
+    if 0 < index < len(grid) - 1 and (ei < len(row)):
+        adjacent_cells = [grid[index - 1][max(0, si - 1): min(ei + 2, len(row))],
+                          grid[index][max(0, si - 1): min(ei + 2, len(row))],
+                          grid[index + 1][max(0, si - 1): min(ei + 2, len(row))]]
+        for row_cells in adjacent_cells:
+            for cell in row_cells:
+                if not (cell.isdigit() or cell == '.'):
+                    return True
+    elif index == 0:
+        adjacent_cells = [grid[index][max(0, si - 1): min(ei + 2, len(row))],
+                          grid[index + 1][max(0, si - 1): min(ei + 2, len(row))]]
+        for row_cells in adjacent_cells:
+            for cell in row_cells:
+                if not (cell.isdigit() or cell == '.'):
+                    return True
+    elif index == len(grid) - 1:
+        adjacent_cells = [grid[index - 1][max(0, si - 1): min(ei + 2, len(row))],
+                  grid[index][max(0, si - 1): min(ei + 2, len(row))]]
+        for row_cells in adjacent_cells:
+            for cell in row_cells:
+                if not (cell.isdigit() or cell == '.'):
+                    return True
 
-def find_parts_in_gears(line):
-    parts_in_gears = {}
-    current_number = ""
-    for i, char in enumerate(line):
-            if char.isdigit():
-                current_number += char
-            elif current_number:
-                if len(current_number) == 3:
-                    key = i - 2
-                    parts_in_gears[key] = current_number
-                    current_number = ''
-                if len(current_number) == 2:
-                    key = i - 1
-                    parts_in_gears[key] = current_number
-                    current_number = ''
-                if len(current_number) == 1:
-                    key = i
-                    parts_in_gears[key] = current_number
-                    current_number = ''
-    return parts_in_gears
+    return False
 
+""" Program initates Below """
 
-def line_contains_symbol(line):
-    symbol_in_gears = {}
-    line_to_check = line.strip()
-    for i,  char in enumerate(line_to_check):
-        if not (char.isdigit() or char == '.'):
-            key = i
-            symbol_in_gears[key] = char
+grid = []
+for line in lines:
+    row = list(line.strip())
+    grid.append(row)
 
-    return symbol_in_gears
-
-
-def sort_to_compare_lines(lines):
-    for line in lines:
-        dict_parts = find_parts_in_gears(line)
-        dict_symbols = line_contains_symbol(line)
-        combined_dict = dict_parts | dict_symbols
-        sorted_dict = sorted(combined_dict.items())
-        print(sorted_dict)
-
-
-
-
-
-
-# for line in lines:
-#     dict_parts = find_parts_in_gears(line)
-#     dict_symbols = line_contains_symbol(line)
-#     combined_dict = dict_parts | dict_symbols
-#     sorted_dict = sorted(combined_dict.items())
-#     print(sorted_dict)
-
+part_numbers = []
+current_number = ''
+for index, row in enumerate(grid):
+    for i, item in enumerate(row):
+        if item.isdigit():
+            current_number += item
+            end_index = i
+            start_index = end_index - len(current_number) + 1
+        elif current_number:
+            validity = look_for_symbols(start_index, end_index, index)
+            if validity:
+                part_numbers.append(current_number)
+                current_number = ''
+            else:
+                current_number = ''
+ans = 0
+for part_number in part_numbers:
+    print(f"{part_number} to add")
+    ans += int(part_number)
+    print(f"{ans} = new total")
 
